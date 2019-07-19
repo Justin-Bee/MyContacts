51c33c7abd0fdbbe4eeddcba1ce38fc39bbb51d4
Justin Bee <jbee.appz@gmail.com>
2019-07-19 10:36:48 -0400

    Update ContactInfoAdapter.kt

--- app/src/main/java/com/bee/mycontacts/ContactInfoAdapter.kt
+++ app/src/main/java/com/bee/mycontacts/ContactInfoAdapter.kt
@@ -76,6 +76,10 @@ class ContactInfoAdapter internal constructor(
 
             }
             //TODO add button click for linkedinView
+            contactLinkedinView.setOnClickListener{
+                val linkedin = contactFound.linkedin
+                
+            }
         }
 
     }
